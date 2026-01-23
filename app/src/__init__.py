163f8339248e0ee9ad926c35a1becf3bed5e7a04
YuraDnistriansky <yura.dniester@gmail.com>
2026-01-23 18:21:05 +0200

    add config

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -4,6 +4,10 @@ from .route import *
 def create_app() -> FastAPI:
     app: FastAPI = FastAPI()
 
+    init_db(app)
     connect_router(app)
 
     return app
+
+def init_db(app: FastAPI) -> None:
+    pass
