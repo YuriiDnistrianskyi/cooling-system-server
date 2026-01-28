55ca83b9c63f9e878bd5c7f4c805f0925e25b202
YuraDnistriansky <yura.dniester@gmail.com>
2026-01-28 23:09:30 +0200

    Create __init__.py

---
+++ app/service/__init__.py
@@ -0,0 +1,7 @@
+from orders.user_service import UserService
+from orders.object_service import ObjectService
+from orders.device_service import DeviceService
+
+user_service = UserService()
+object_service = ObjectService()
+device_service = DeviceService()
