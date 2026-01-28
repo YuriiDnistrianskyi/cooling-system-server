1930d1299319ee50784d5e94779b685814a0fc1f
YuraDnistriansky <yura.dniester@gmail.com>
2026-01-28 23:07:35 +0200

    replacement types

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, EmailStr
 
 class CreateUser(BaseModel):
     firstName: str
     lastName: str
-    email: str
+    email: EmailStr
     password: str
 
 class UpdateUser(BaseModel):
     firstName: Optional[str] = None
     lastName: Optional[str] = None
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
     password: Optional[str] = None
