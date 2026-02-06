617654f2d0423be5149223378a04bdc17ae3ee21
YuraDnistriansky <yura.dniester@gmail.com>
2026-02-06 19:15:45 +0200

    Update ws_manager.py

--- app/ws/ws_manager.py
+++ app/ws/ws_manager.py
@@ -2,16 +2,28 @@ from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self) -> None:
-        self.active_connections: dict[str, WebSocket] = {}
+        self.device_connections: dict[str, WebSocket] = {}
+        self.object_connections: dict[str, WebSocket] = {}
 
-    async def connect(self, device_id: str, websocket: WebSocket) -> None:
+    async def connect(self, type: str, id: str, websocket: WebSocket) -> None:
         await websocket.accept()
-        self.active_connections[device_id] = websocket
+        if type == "device":
+            self.device_connections[id] = websocket
+        elif type == "object":
+            self.object_connections[id] = websocket
 
-    async def disconnect(self, device_id: str, websocket: WebSocket) -> None:
-        self.active_connections.pop(device_id, None)
+    async def disconnect(self, type: str, id: str, websocket: WebSocket) -> None:
+        if type == "device":
+            self.device_connections.pop(id)
+        elif type == "object":
+            self.object_connections.pop(id)
 
-    async def send_to(self, device_id: str, massage: dict) -> None:
-        ws = self.active_connections.get(device_id, None)
+    async def send_to(self, type: str, id: str, massage: dict) -> None:
+        ws = None
+        if type == "device":
+            ws = self.device_connections.get(id, None)
+        elif type == "object":
+            ws = self.object_connections.get(id, None)
+
         if ws:
             await ws.send_json(massage)
