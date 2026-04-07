f2a14745e74d3a8ceeb12705ca4246cd03b53aae
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-07 21:55:42 +0300

    fixs

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = getenv("DATABASE_URL")
-# MONGO_DB_URL = getenv("MONGO_DB_URL")
 INFLUXDB_URL = getenv("INFLUXDB_URL")
 INFLUXDB_TOKEN = getenv("INFLUXDB_TOKEN")
 INFLUXDB_ORG = getenv("INFLUXDB_ORG")
